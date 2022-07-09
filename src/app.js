2b08790473ebf4543fcf521982dee478e7417953
Katheren Salom <katherensalom@gmail.com>
2022-07-09 21:12:06 +0000

    se esta ajustando el codigo en el archivo   src/app.js se coloco un br en el html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let sustant = ["jogger", "racoon"];
+  let extensions = ["com", "es", "net", "io", "us"];
+
+  document
+    .querySelector(".dominio")
+    .classList.add(GenerarDominios(pronoun, adj, sustant, extensions));
 };
+
+function GenerarDominios(pronoun, adj, sustant, extensions) {
+  let pronunciacion = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let adjetivo = adj[Math.floor(Math.random() * adj.length)];
+  let sustantivo = sustant[Math.floor(Math.random() * sustant.length)];
+  let extension = extensions[Math.floor(Math.random() * extensions.length)];
+  let domin = pronunciacion + "" + adjetivo + "" + sustantivo + "." + extension;
+
+  //return domin[domin];
+  console.log(domin);
+}
