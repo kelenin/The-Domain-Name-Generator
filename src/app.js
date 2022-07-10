c1f1c2e79fddd625c3dfc1f978755b65a3fdbbdd
Katheren Salom <katherensalom@gmail.com>
2022-07-10 00:55:34 +0000

    se ajusto el codigo

--- src/app.js
+++ src/app.js
@@ -1,34 +1,40 @@
 /* eslint-disable */
 import "./style.css";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let sustant = ["jogger", "racoon"];
   let extensions = ["com", "es", "net", "io", "us"];
 
-  document.querySelector(".dominio").innerHTML = GenerarDominios(
-    pronoun,
-    adj,
-    sustant,
-    extensions
-  );
+  const collection = document.getElementsByClassName("dominio");
+  collection.innerHTML = GenerarDominios(pronoun, adj, sustant, extensions);
 };
 
 function GenerarDominios(pronoun, adj, sustant, extensions) {
-  let pronunciacion = Math.floor(Math.random() * pronoun.length);
-  let adjetivo = Math.floor(Math.random() * adj.length);
-  let sustantivo = Math.floor(Math.random() * sustant.length);
-  let extension = Math.floor(Math.random() * extensions.length);
-  let domin =
-    pronoun[pronunciacion] +
-    "" +
-    adj[adjetivo] +
-    "" +
-    sustant[sustantivo] +
-    "." +
-    extensions[extension];
+  var element = document.querySelector(".dominio");
 
-  return domin;
+  for (let i = 0; i <= 8; i++) {
+    let pronunciacion = Math.floor(Math.random() * pronoun.length);
+    let adjetivo = Math.floor(Math.random() * adj.length);
+    let sustantivo = Math.floor(Math.random() * sustant.length);
+    let extension = Math.floor(Math.random() * extensions.length);
+    let domin =
+      pronoun[pronunciacion] +
+      "" +
+      adj[adjetivo] +
+      "" +
+      sustant[sustantivo] +
+      "." +
+      extensions[extension];
+
+    let datoParrafo = document.createElement("p");
+
+    datoParrafo.innerHTML = domin;
+
+    element.appendChild(datoParrafo);
+
+    //console.log(domin);
+  }
 }
